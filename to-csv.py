e760691958007947982e965c9ea5453be24d4ae4
Key Paul <kira.paulfitton@gmail.com>
2020-11-25 22:46:58 +0000

    Add files via upload
    formatted data structure to only value as item of the correct type, TODO refactor - could remove at least one fore each

--- to-csv.py
+++ to-csv.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd #wireframe
 #from pd import ExcelWriter as ew
 
+# replaces the items data for where the plans are unsuccesful
 def replaceFail(stats, code):
     for num, info in enumerate(stats):
         if num == 18:
@@ -12,7 +13,7 @@ def replaceFail(stats, code):
             stats[num-1] = None
     #stats=stats[:-1]
 
-
+# reads files from txt to array, call to organise successful plans and replace array info for unsuccesful plans
 def readFile(name):
     file = open("plans/"+name, "r")
     read_file = file.readlines()
@@ -21,23 +22,24 @@ def readFile(name):
     if ": 0" in stats[len(stats)-2]:
         print("plan success")
         stats = stats[:21]
-        # TODO should run organise from here? (so that only for successful plans as the failed plans to be organised below)
-        stats = organise(stats)
-        print(stats[20])
+        stats = organise(stats)  # call organise from here? (so that only for successful plans as the failed plans to be organised below)
+        #print("::", enumerate(stats))
+        #temp = remove_front_back("search exit code: 7102", 18, len("search exit code: 1072"))
+        #print("<", temp, ">")
+        remove_fluff(stats) #TODO
+
     elif ": 22" in stats[len(stats)-3]:
         print("plan fail - memory")
-        replaceFail(stats, "search exit code: 22")
+        replaceFail(stats, 22)
     elif ": 23" in stats[len(stats)-3]:
         print("plan fail - time")
-        replaceFail(stats, "search exit code: 23")
+        replaceFail(stats, 23)
 
     #stats=stats[:-1]
     return stats
 
-
+# removes redundant infor of timestep at the beginning and/or the whitespace at the end 
 def organise(stats):
-    #remove first 24 characters f each item
-    #TODO organise the failed planners to be nan for all fields except "solution found?" and "exit code"
     for num, info in enumerate(stats):
         #print("this: ", i) #i of type str
         #print(num, ": ", info)
@@ -45,12 +47,81 @@ def organise(stats):
         if num < 17:
             search = info.find("] ")
             start_search = search+2
-            update = info[start_search:-1] #removes "\n"
+            update = info[start_search:-1] # makes character selction  
         else:
             update = info[:-1]
 
         stats[num] = update
     #print(stats)
+    return stats
+
+
+def remove_front_back(data, front, back):
+    data = data[front:back]
+    #print("::", data)
+    return data
+
+
+def switcher(col_val, data):
+    switch={
+        0: data[13:-9], #remove_front_back(data, 13, -9), # Plan length
+        1: data[11: len(data)], #Plan cost
+        2: data[9 : -10], # Expanded state(s)
+        3: data[9 : -10], # Reopened state(s)
+        4: data[10 : -10], # Evaluated state(s)
+        5: data[13 : len(data)], # Evaluations
+        6: data[10 : -10], # Generates state(s)
+        7: data[11 : -10], # Deadends 
+        8: data[26 :-10], # Expanded until last jump
+        9: data[26 : -10], # Reopened until last jump
+        10: data[27 : -10], # Evaluated until last jump
+        11: data[27 : -10], # Generated until last jump
+        12: data[29 : len(data)], # Number of registered states
+        13: data, # Int hash set load factor
+        14: data[22 : len(data)], # Int hash set resizes
+        15: data[13 : -1], # Search time
+        16: data[12 : -1], # Total time
+        17: data, # Solution found?, string: found vs not found
+        18: data[13 : -3], # Peak memory
+        19: data, # Remove intermediate file output.sas, string
+        20: data[18 : len(data)] # Search exit code
+    }
+    return switch.get(col_val, None)
+
+
+# TODO 
+def remove_fluff(stats):
+    for col, data in enumerate(stats):
+        update = ""
+        if int(col) == 13:
+            search = data.find("= ")
+            start_search = search+2 #for "= "
+            end = len(data)
+            update = float(data[start_search:end])
+            print("--Updated: ", update, ", Type: ", type(update))
+        elif int(col) in [15, 16]: #floats
+            n_data = switcher(col, data)
+            update = float(n_data)
+            print("--Updated: ", update, ", Type: ", type(update))
+        elif int(col) in [17, 19]:
+            update = str(data)
+            print("--Updated: ", update, ", Type: ", type(update))
+            #print("Helloooooo")
+        else:
+            n_data = switcher(col, data) 
+            #print("New data: ", n_data)
+            update = int(n_data) #string
+            print("--Updated: ", n_data, ", Type: ", type(update))
+
+        stats[col] = update
+    
+    print(stats)
+
+    """
+    for row in enumerate(stats):
+        for col, info_item in enumerate(stats):
+            stats = switcher(col, info_item)
+    """      
     return stats
 
 
@@ -82,7 +153,6 @@ print(data[1])
 """
 
 data = []
-"""
 file_names = ["add-inst1.txt", "add-inst2.txt",
                 "add-inst3.txt", "add-inst4.txt",
                 "add-inst5.txt", "add-inst6.txt",
@@ -105,7 +175,7 @@ file_names = ["mas-inst1.txt", "mas-inst2.txt",
                 "mas-inst15.txt", "mas-inst16.txt",
                 "mas-inst17.txt", "mas-inst18.txt",
                 "mas-inst19.txt", "mas-inst20.txt"] # planner for instance 19 & 20 failed
-
+"""
 
 for i in file_names:
     stats = readFile(i)
@@ -140,15 +210,23 @@ df = pd.DataFrame(data)
 #df_t = df.transpose()
 #print(df_t)
 
+
+"""
+for index, row in df.items(): #iterrows
+    #df.at[i, 0] = content[12:]
+    print("--, ", index)
+"""
 #rename colums
-df_rn = df.rename(columns={0:"Plan length", 1: "Plan cost", 2:"Expanded state(s)", 3:"Reopened state(s)", 4:"Evaluates state)s)", 5:"Evaluations", 6:"Generates state(s)", 7:"Deadends", 8:"Expanded until last jump", 9:"Reopened until last jump", 10:"Evaluated until last jump", 11:"Generated until last jump", 12:"Number or registeres states", 13:"Int hash set load factor", 14:"Int hash set resizes", 15:"Search time", 16:"Total time", 17: "Solution found?", 18:"Peak memory", 19:"Remove intermediate file output.sas", 20:"Search exit code"}) #, index={0:"instance 1"})
+df_rn = df.rename(columns={0:"Plan length", 1: "Plan cost", 2:"Expanded state(s)", 3:"Reopened state(s)", 4:"Evaluated state(s)", 5:"Evaluations", 6:"Generates state(s)", 7:"Deadends", 8:"Expanded until last jump", 9:"Reopened until last jump", 10:"Evaluated until last jump", 11:"Generated until last jump", 12:"Number of registered states", 13:"Int hash set load factor", 14:"Int hash set resizes", 15:"Search time", 16:"Total time", 17: "Solution found?", 18:"Peak memory", 19:"Remove intermediate file output.sas", 20:"Search exit code"}) #, index={0:"instance 1"})
 #print("---------")
 
 #print(df_rn)
 
 # Write df to excel
 #writer = ew()
-df_rn.to_excel("plans/output-mas.xlsx")
+df_rn.to_excel("plans/output.xlsx")
+
+
 
 ""
 print("------- first")
